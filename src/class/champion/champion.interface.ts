d719be9580157ce3dcd6de0c2a4a569cfca87cb5
Vincent <31652038+Vincent-Manach@users.noreply.github.com>
2021-12-03 14:46:11 +0100

    Handle Champion class

--- src/class/champion/champion.interface.ts
+++ src/class/champion/champion.interface.ts
@@ -1,1 +1,12 @@
-export default interface IChampion {}
+import { ChampionType } from "./championtype.enum";
+import Champion from "./champion.class";
+
+
+export default interface IChampion {
+    name: string;
+    health: number;
+    type: ChampionType;
+    protection: boolean;
+    protect(): void;
+    attack(ennemy: Champion): void;
+}
