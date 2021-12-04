65094dafde816a7e755e58252818d4724aeab8f3
Gautier <hello.gautiermaire@gmail.com>
2021-12-04 15:31:50 +0100

    Add test champion > coverage 100% passed + fix champion class and child :white_check_mark: :beers:

--- src/champion/champion.class.ts
+++ src/champion/champion.class.ts
@@ -21,11 +21,17 @@ export default class Champion implements IChampion{
     attack(enemy: Champion): void {
         if (enemy.health > 0) {
             if (enemy.protection == false) {
-                enemy.health -= 50
+                enemy.health -= 100;
             } else {
+                if (enemy.type == ChampionType.Warrior) {
+                    // pas de dégât
+                    enemy.health -= 0;
+                } else {
+                    // dégâts / 2
+                    enemy.health -= 50;
+                }
                 enemy.protection = false;
             }
         }
-        enemy.health -= 50;
     }
 }
--- test/champion.test.ts
+++ test/champion.test.ts
@@ -1,4 +1,49 @@
 import Champion from '../src/champion/champion.class';
+import { ChampionType } from "../src/champion/championtype.enum";
+
+describe("champion", ()=> {
+  let champion1: Champion;
+  let champion2: Champion;
+  let champion3: Champion;
+
+  beforeEach( () => {
+    champion1 = new Champion("Gautier", ChampionType.Witcher, 1000, false);
+    champion2 = new Champion("Vincent", ChampionType.Warrior, 1000, false);
+    champion3 = new Champion("Baptiste", ChampionType.Archer, 1000, false);
+  });
+  
+
+  test("ne peut pas infliger de dégats à l'ennemi s'il a plus de vie", ()=> {
+    champion2.health = 0;
+    champion1.attack(champion2);
+    expect(champion2.health).toBe(0);
+  });
+
+  test("si l'ennemi n'as pas de protection, on inflige les dégats classiques", ()=> {
+    champion1.attack(champion3);
+    expect(champion3.health).toBe(900);
+  });
+
+  test("si le champion se protège alors il est protégé (protection à true", ()=> {
+    champion1.protect();
+    expect(champion1.protection).toBe(true);
+  });
+
+  test("si l'ennemi se protège et ennemi différent de warrior, on inflige moitié moin de dégats et l'ennemi n'est plus protégé", ()=> {
+    champion3.protect();
+    champion1.attack(champion3);
+    expect(champion3.health).toBe(950);
+    expect(champion3.protection).toBe(false);
+  });
+
+  test("si l'ennemi se protège et ennemi est du type warrior, on inflige aucun dégat et l'ennemi n'est plus protégé", ()=> {
+    champion2.protect();
+    champion1.attack(champion2);
+    expect(champion2.health).toBe(1000);
+    expect(champion2.protection).toBe(false);
+  });
+
+});
 
 // Exemples
 // import {BankAccount} from '../src/bankAccount';
